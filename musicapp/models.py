6efbce38aefaddf098140490e0fc77c939aa634f
Segun Oladimeji <segunoladimeji@seguns-mbp.speedport_w_723v_1_49_000>
2022-10-25 15:35:40 +0200

    Updated

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,35 @@
+from email.policy import default
+from platform import release
+from pyexpat import model
+from tkinter import CASCADE
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 40)
+    last_name = models.CharField(max_length = 40)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name 
+    
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+    artist_id = models.ForeignKey(Artiste, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Lyrics(models.Model):
+    content = models.CharField(max_length=1000)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.content
